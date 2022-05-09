32af7fe0920d08485a5b98c1b9d73552e03e2cba
Rhizlaine DEGNI <36238355+RhizlaineDegni@users.noreply.github.com>
2022-05-09 14:46:07 +0200

    Update edit.php

--- edit.php
+++ edit.php
@@ -72,13 +72,7 @@ $conn = sqlsrv_connect($serverName, $connectionInfo);
         <section class="py-5">
             <div class="container px-4 px-lg-5 mt-5">
                 <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-                <?php     
-                <?php     
 
-                $result = sqlsrv_query($conn, "select * from librairie");
-                while ($row =sqlsrv_fetch_array($result, SQLSRV_FETCH_NUMERIC))  {  }
-pg_close($conn);
-?>
 
 
                 </div>
